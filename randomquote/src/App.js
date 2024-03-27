cfd961ccd3cb82ad5b2e1acaf05940c5b8f34959
alp <alptuzer@gmail.com>
2024-03-27 15:51:09 +0300

    second

--- randomquote/src/App.js
+++ randomquote/src/App.js
@@ -1,11 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
+import "./app.css"
  
 function  App() {
  
+  const [quotes,setQuotes] = useState("");
+  const getQutes = ()  =>{
+
+         fetch("https://type.fit/api/quotes")
+         .then((res) => res.json())
+        /*  .then((data) => console.log(data)); */
+        .then((data) =>{let randomNum = Math.floor(Math.random()*data.length)
+          setQuotes(data[randomNum])
+        
+        })
+      }
+
+      useEffect(()=> { getQutes();} ,[])
+     
+
     return (
       <div className="App">
  
-  <div>Deneme</div>
+ <div className='quote'>
+  <p>{quotes.text}</p>
+  <p>{quotes.author}</p>
+ 
+
+ <div className='btnContainer'>
+  <button onClick={getQutes} className='btn' > Get Quote</button>
+ </div>
+
+ </div>
+
       </div>
     );
   }
